ccf9a19c8a055f1f4c2ca1b3d0357701a2deae80
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-09-20 17:25:50 -0300

    feat: cria model da tabela sales

---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,63 @@
+const { DataTypes } = require('sequelize');
+
+const attributes = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  userId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Users',
+      key: 'id'
+    },
+    field: 'user_id'
+  },
+  sellerId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Users',
+      key: 'id'
+    },
+    field: 'seller_id'
+  },
+  totalPrice: {
+    allowNull: false,
+    type: DataTypes.DECIMAL(9, 2),
+    field: 'total_price'
+  },
+  deliveryAddress: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    field: 'delivery_address'
+  },
+  deliveryNumber: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    field: 'delivery_number'
+  },
+  saleDate: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'sale_date'
+  },
+  status: {
+    allowNull: false,
+    type: DataTypes.STRING
+  }
+}
+
+module.exports = (sequelize) => {
+  const model = sequelize.define('Sale', attributes, { tableName: 'Sales', timestamps: false })
+  model.associate = (models) => {
+    models.Sale.belongsTo(models.User, { 
+      through: model,
+      foreignKey: 'id',
+    });
+  }
+  return model;
+};
