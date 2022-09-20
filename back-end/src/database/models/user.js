0c22eb2f4a5ccf728d58b3cb8958a1fd3278c3c9
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-09-20 19:57:10 -0300

    fix: corrige models

--- back-end/src/database/models/user.js
+++ back-end/src/database/models/user.js
@@ -27,15 +27,5 @@ const attributes = {
 
 module.exports = (sequelize) => {
   const model = sequelize.define('User', attributes, { tableName: 'Users', timestamps: false })
-  model.associate = (models) => {
-    models.User.hasMany(models.Sale, { 
-      through: model,
-      foreignKey: 'userId',
-    });
-    models.User.hasMany(models.Sale, { 
-      through: model,
-      foreignKey: 'sellerId', 
-    });
-  }
   return model;
 };
