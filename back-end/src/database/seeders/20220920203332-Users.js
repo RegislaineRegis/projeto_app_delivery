ecc1d60332e5cf28ef8dd6a1d3a7eb1c288978dd
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-09-20 17:36:41 -0300

    feat: adiciona seeders para a tabela products

--- back-end/src/database/seeders/20220920203332-Users.js
+++ back-end/src/database/seeders/20220920203332-Users.js
@@ -2,23 +2,29 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Users', [
+      {
+        name: 'Delivery App Admin', 
+        email: 'adm@deliveryapp.com', 
+        password: 'a4c86edecc5aee06eff8fdeda69e0d04', 
+        role: 'administrator'
+      },
+      {
+        name: 'Fulana Pereira', 
+        email: 'fulana@deliveryapp.com', 
+        password: '3c28d2b0881bf46457a853e0b07531c6', 
+        role: 'seller'
+      },
+      {
+        name: 'Cliente Zé Birita', 
+        email: 'zebirita@email.com', 
+        password: '1c37466c159755ce1fa181bd247cb925', 
+        role: 'customer'
+      },
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Users', null, {});
   }
 };
