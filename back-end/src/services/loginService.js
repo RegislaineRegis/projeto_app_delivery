6bfdbb2a5c23e2615197d803a8bbaea825eaa38c
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-09-20 19:58:33 -0300

    feat: adiciona service do login

---
+++ back-end/src/services/loginService.js
@@ -0,0 +1,19 @@
+const models = require('../database/models');
+const { throwCustomError } = require('./utils');
+
+const getUser = async (body) => {
+  const { email, encryptedPass: password } = body;
+  const check = await models.User.findOne({ where: { email, password }, raw: true });
+  return check;
+};
+
+const validateBody = (body) => {
+  const { email, password } = body;
+  if (!email || !password) return throwCustomError(400, 'Invalid fields');
+  const regex = /\S+@\S+.\S+/;
+  const validateEmail = regex.test(email);
+  const validatePassword = password.length > 6;
+  if (!validateEmail || !validatePassword) return throwCustomError(400, 'Invalid Fields');
+};
+
+module.exports = { getUser, validateBody };
