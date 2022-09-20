c5fd76f86e83de44e498cdaae563c78625658778
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-09-20 17:24:16 -0300

    feat: cria migration da tabela sales

---
+++ back-end/src/database/migrations/20220920191941-create-sales.js
@@ -0,0 +1,58 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        field: 'user_id'
+      },
+      sellerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        field: 'seller_id'
+      },
+      totalPrice: {
+        allowNull: false,
+        type: Sequelize.DECIMAL(9, 2),
+        field: 'total_price'
+      },
+      deliveryAddress: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'delivery_address'
+      },
+      deliveryNumber: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'delivery_number'
+      },
+      saleDate: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'sale_date'
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Sales');
+  }
+};
