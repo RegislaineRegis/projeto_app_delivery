d34f059dc5159af293cd0c1bcc7fd39a81f843bf
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-09-21 18:03:40 -0300

    feat: adiciona rota login e redireciona rota / para ela

---
+++ front-end/src/provider/provider.js
@@ -0,0 +1,23 @@
+// import React, { createContext, useEffect, useState } from 'react';
+// import PropTypes from 'prop-types';
+
+// export const Context = createContext();
+
+// function Provider({ children }) {
+//   const [state, setState] = useState({
+//   });
+
+//   const value = {};
+
+//   return (
+//     <Context.Provider value={ value }>
+//       {children}
+//     </Context.Provider>
+//   );
+// }
+
+// Provider.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
+
+// export default Provider;
