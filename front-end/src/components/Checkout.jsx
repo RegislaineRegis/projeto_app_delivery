af4e992bbd7800b164a81ebe7dff29d4a3027add
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-09-26 19:05:00 -0300

    feat: adiciona rota /customer/checkout

---
+++ front-end/src/components/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CheckoutTable from './CheckoutTable';
+import CheckoutForm from './CheckoutForm';
+
+export default function Checkout() {
+  return (
+    <main>
+      <CheckoutTable />
+      <CheckoutForm />
+    </main>
+  );
+}
---
+++ front-end/src/pages/CustomerCheckout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Checkout from '../components/Checkout';
+
+export default function CustomerCheckout() {
+  return (
+    <div>
+      <NavBar />
+      <Checkout />
+    </div>
+  );
+}
